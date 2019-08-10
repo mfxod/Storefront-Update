a56231b04b30c221c9049118234551ba75ca1bcc
mfxod <46651745+mfxod@users.noreply.github.com>
2019-08-10 17:36:38 -0400

    routing

--- routes.js
+++ routes.js
@@ -2,16 +2,12 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const exphbs = require("express-handlebars")
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}))
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 
 const mysql = require("mysql");
@@ -47,31 +43,36 @@ app.get("/", (req, res) => {
     })
 })
 
-// PUT to place order
-// Q: What should the route path be? Can't they all be "/"?
-app.put("/", (req, res) => {
-    // if order-quantity > stock_quantity show "Insufficient Stock" message, otherwise do this:
-    // Q: Use req.body or req.params?
-    // Q: Can I do math in the query like this?
-    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?", [req.body.order-quantity, req.body.id], (err, result) => {
+// POST to place order
+app.post("/api/order", (req, res) => {
+    connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [req.body.id], (err, result) => {
         if (err) {
             console.log(err)
             return res.status(500).send("Niet, Smirnoff.")
         }
 
-        if (!result.changedRows) {
-            console.log(err)
-            return res.status(404).send("Suzie, do you copy?... Suzie?")
-        }
+        const difference = result[0].stock_quantity - req.body.order_quantity
 
-        console.log("order quantity: " + req.params.order-quantity)
-        console.log("order id: " + req.params.id)
-        return res.json({ OK: true })
+        // need to show order total with handlebars
+        // const orderTotal = result[0].price * req.body.order_quantity
+
+        if (difference < 0) {
+            // this message needs to be displayed on page through handlebars somehow
+            console.log("Order quantity must be less than In Stock quantity.")
+        } else {
+            connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [difference, req.body.id], (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).send("Niet, Smirnoff.")
+                }
+
+                return res.json(result)
+            })
+        }
     })
 })
 
 // DELETE to remove product once stock_quantity === 0
-
 
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT)

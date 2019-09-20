1b0e5552dae718c24be9640c685734b100151cac
Matt <mfxod828@gmail.com>
2019-09-20 12:26:17 -0400

    added JAWSDB

--- routes.js
+++ routes.js
@@ -11,13 +11,19 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "bamazondb"
-});
+
+if(process.env.JAWSDB_URL) {
+    connection = createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "bamazondb"
+    })
+}
+
 
 connection.connect(err => {
     if (err) {

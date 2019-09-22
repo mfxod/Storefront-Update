a9c748e76ee6bfc1e3ef813d98af25292bbbeef8
Matt <mfxod828@gmail.com>
2019-09-22 16:13:51 -0400

    modularized server and routes

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const router = express.Router()
+
+// GET to display products
+app.get("/", (req, res) => {
+    connection.query("SELECT * FROM products", (err, data) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).send("Niet, Smirnoff.")
+        }
+        res.render("index", { products: data })
+    })
+})
+
+// POST to place order
+app.post("/api/order", (req, res) => {
+    connection.query("SELECT stockQuantity FROM products WHERE itemID = ?", [req.body.id], (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).send("Niet, Smirnoff.")
+        }
+
+        const difference = result[0].stockQuantity - req.body.order_quantity
+
+        if (difference < 0) {
+            console.log(difference)
+            connection.query("SELECT * FROM products", (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).send("Niet, Smirnoff.")
+                }
+                
+                console.log("Making it this far.")
+                // console.log(data)
+                // render doesn't work...
+                res.render("index", { message: "Order quantity must be less than In Stock quantity.", products: data })
+                console.log("How about here?")
+            })
+
+        } else {
+            connection.query("UPDATE products SET stockQuantity = ? WHERE itemID = ?", [difference, req.body.id], (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).send("Niet, Smirnoff.")
+                }
+                return res.json(result)
+            })
+        }
+    })
+})
+
+// DELETE to remove product once stock_quantity === 0
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const routes = require("./routes")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const exphbs = require("express-handlebars")
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+const mysql = require("mysql");
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "bamazondb"
+    })
+}
+
+connection.connect(err => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return
+    }
+    console.log("connected as id " + connection.threadId)
+});
+
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT)
+});
